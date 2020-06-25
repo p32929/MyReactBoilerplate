5ec0f7360e4ecedf31fcb7bc496bbfc99b9d0c30
p32929 <p32929@yahoo.com>
2020-06-25 13:08:19 +0600

    fixed readme

---
+++ src/Components/CounterDemo.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Grid from "@material-ui/core/Grid";
+import {useOvermind} from "../Others/OvermindHelper";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+
+const CounterDemo = () => {
+    const {state, actions} = useOvermind()
+
+    return (
+        <Grid style={{padding: 48}} container direction='column' justify='center' alignItems='center'
+              alignContent='center'>
+            <Typography>{state.counter}</Typography>
+            <Grid container direction='row' alignItems='center' alignContent='center' justify='center'>
+                <Button style={{margin: 8}} color='primary' variant='contained' onClick={() => {
+                    actions.increase(1)
+                }}>+</Button>
+                <Button style={{margin: 8}} color='primary' variant='contained' onClick={() => {
+                    actions.increase(-1)
+                }}>-</Button>
+            </Grid>
+        </Grid>
+    );
+};
+
+export default CounterDemo;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Provider} from "overmind-react";
+import {overmind} from './Others/OvermindHelper'
+import CounterDemo from "./Components/CounterDemo";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider value={overmind}>
+            <CounterDemo/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change

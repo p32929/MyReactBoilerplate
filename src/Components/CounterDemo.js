1a602614016f4efff52d9899a3879de7435262ea
Fayaz Bin Salam <p32929@users.noreply.github.com>
2020-09-23 20:34:37 +0600

    Update CounterDemo.js
    added classes

--- src/Components/CounterDemo.js
+++ src/Components/CounterDemo.js
@@ -6,12 +6,13 @@ import Button from "@material-ui/core/Button";
 import { makeStyles } from '@material-ui/core/styles';
 import {theme} from "../Others/Theme";
 
-const useStyle = makeStyles((theme) => ({
-
+const useStyles = makeStyles((theme) => ({
+    //
 }))
 
 const CounterDemo = () => {
     const {state, actions} = useOvermind()
+    const classes = useStyles();
 
     return (
         <Grid style={{padding: 48}} container direction='column' justify='center' alignItems='center'
